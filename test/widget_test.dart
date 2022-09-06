9d4ec6e96673c6e98afa8c4d679e573b6c8eddcf
dfpolo2022 <dfpolo@uninorte.edu.co>
2022-09-05 22:33:58 -0500

    cambios uno

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:math';
+
+import 'package:f_testing_template/ui/pages/authentication/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
 
 void main() {
-  testWidgets(
-      'Widget login validación @ email', (WidgetTester tester) async {});
+  testWidgets('Widget login validación @ email', (WidgetTester tester) async {
+    await tester.pumpWidget(const GetMaterialApp(
+        home: LoginScreen(
+      key: Key('LoginScreen'),
+      email: "blank",
+      password: "blank",
+    )));
+
+    expect(find.byKey(const Key('LoginScreen')), findsOneWidget);
+  });
 
   testWidgets('Widget login validación campo vacio email',
       (WidgetTester tester) async {});
